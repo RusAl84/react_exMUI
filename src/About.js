a7937a7cd54487d357bbb292f44986458845faf5
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-11-11 19:30:51 +0300

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import Button from '@mui/material/Button';
+
+export default class About extends Component {
+
+    render() {
+        return(
+            <Box>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+                <Button variant="contained">Contained</Button>
+                <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                </Box>
+            </Box>
+        )
+    }
+}
